ac06e372cb046c166fbfabebdc4f0740922f391f
prajjwal2-3 <121849327+prajjwal2-3@users.noreply.github.com>
2024-03-03 23:51:33 +0530

    sponsors done

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ImagesSliderDemo from './ImgSlider'
 import  SparklesPreview  from './Hero'
+import Sponser from './Sponser'
 
 const Body = () => {
   return (
@@ -8,6 +9,7 @@ const Body = () => {
       <div className="w-8/12 sm:m-2 m-2 ">
         <div className="flex justify-center"><SparklesPreview/></div>
      <ImagesSliderDemo/>
+     <div className="flex justify-center"><Sponser/></div>
       </div>
     </div>
   )
