32c374e68ac3ed76f8595759ec361a37b08cdb2c
prajjwal2-3 <121849327+prajjwal2-3@users.noreply.github.com>
2024-03-03 20:22:49 +0530

    Hero done

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ImagesSliderDemo from './ImgSlider'
+import  SparklesPreview  from './Hero'
 
 const Body = () => {
   return (
     <div className='flex justify-center'>
       <div className="w-8/12 sm:m-2 m-2 ">
+        <SparklesPreview/>
      <ImagesSliderDemo/>
       </div>
     </div>
