f590fe179c6ab096467eeb8a8cab59e2551b32e2
prajjwal2-3 <121849327+prajjwal2-3@users.noreply.github.com>
2024-03-03 18:42:30 +0530

    images-slider

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
 import ResponsiveAppBar from "./Header";
 import GridBackgroundDemo from "./Background"
-
+import ImagesSliderDemo from "./ImgSlider";
 
 function App() {
   return (
     <div className="App   ">
     <ResponsiveAppBar/>
-     
+     <ImagesSliderDemo/>
       <GridBackgroundDemo/>
     </div>
   );
